fb5cf583672f6c8249187c6a6903990d33b3eaa5
ReneAquitania <rene.aquitania120@gmail.com>
2023-03-19 17:15:31 +0800

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="grid">
+      <div className="grid grid-rows-6">
+        <div>
+          <h1 className="text-6xl text-center font-bold border-2">
+            About Us
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-4xl text-center font-bold border-2">
+            Meet the team
+          </h1>
+        </div>
+        <div className="text-3xl text-center font-semibold border-2">
+          Rene Aquitania
+        </div>
+        <div className="text-3xl text-center font-semibold border-2">
+          Adrian Lim
+        </div>
+        <div className="text-3xl text-center font-semibold border-2">
+          Enrico Ramos
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
